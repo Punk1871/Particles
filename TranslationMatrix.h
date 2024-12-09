d916c1da52c54b02c27b0c83c6d3b14dc6f2f497
Punk1871 <eliasabevan@gmail.com>
2024-12-09 12:16:12 -0800

    Delete TranslationMatrix.h

--- TranslationMatrix.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Matrices.h"
-
-using namespace Matrices;
-class TranslationMatrix : public Matrix {
-public:
-    TranslationMatrix(double xShift, double yShift, int nCols);
-};
