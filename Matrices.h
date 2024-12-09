162817d2a93121e4f79c43868600edcb02e69cd1
Punk1871 <eliasabevan@gmail.com>
2024-12-09 12:37:27 -0800

    Update Matrices.h

--- Matrices.h
+++ Matrices.h
@@ -78,13 +78,6 @@ namespace Matrices
             sin(theta)   cos(theta)
             */
             ///theta represents the angle of rotation in radians, counter-clockwise
-        RotationMatrix(double theta) : Matrix(2, 2) 
-        {
-            (*this)(0, 0) = cos(theta);
-            (*this)(0, 1) = -sin(theta);
-            (*this)(1, 0) = sin(theta);
-            (*this)(1, 1) = cos(theta);
-        }
             RotationMatrix(double theta);
     };
 
@@ -100,13 +93,6 @@ namespace Matrices
             0       scale
             */
             ///scale represents the size multiplier
-ScalingMatrix(double scale) : Matrix(2, 2) 
-        {
-            (*this)(0, 0) = scale;
-            (*this)(0, 1) = 0;
-            (*this)(1, 0) = 0;
-            (*this)(1, 1) = scale;
-        }
             ScalingMatrix(double scale);
     };
 
@@ -124,15 +110,6 @@ ScalingMatrix(double scale) : Matrix(2, 2)
             ///paramaters are xShift, yShift, and nCols
             ///nCols represents the number of columns in the matrix
             ///where each column contains one (x,y) coordinate pair
-
-        TranslationMatrix(double xShift, double yShift, int nCols) : Matrix(2, nCols) 
-        {
-            for (unsigned short i = 0; i < nCols; i++) 
-            {
-                (*this)(0, i) = xShift;
-                (*this)(1, i) = yShift;
-             }
-        }
             TranslationMatrix(double xShift, double yShift, int nCols);
     };
 }
