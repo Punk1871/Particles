aebfd1cb67a15e8ca4b2212223e1f127405ac321
Punk1871 <eliasabevan@gmail.com>
2024-12-09 11:34:38 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -85,9 +85,8 @@ void Engine::input()
 }
 
 void Engine::update(float dtAsSeconds)
-{
-	auto it;
-    for ( it = m_particles.begin(); it != m_particles.end();)
+{	auto it = m_particles.begin();
+    for ( it ; it != m_particles.end();)
     {
         if (it->getTTL() > 0.0)
         {
